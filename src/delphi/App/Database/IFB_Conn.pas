c40bdda054c825d34ff145f5ae16163a3725385d
Ismael Faustino <ismael.faustino@esyspark.com.br>
2017-07-07 10:53:44 -0300

    Implementada propriedade que retorna a localização do arquivo de configuração dos bancos de dados.

--- src/delphi/App/Database/IFB_Conn.pas
+++ src/delphi/App/Database/IFB_Conn.pas
@@ -3,17 +3,19 @@ unit IFB_Conn;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, IFB_App;
 
 type
   TIFB_Conn = class
   private
     FConnName: string;
     procedure setConnName(const Value: string);
+    function getDataBaseFileConf: string;
     { Private declarations }
   public
     constructor Create(const ConnName:  string);
     property ConnName : string read FConnName  write setConnName;
+    property DataBaseFileConf : string read getDataBaseFileConf;
     { Public declarations }
   end;
 
@@ -24,6 +26,11 @@ implementation
 constructor TIFB_Conn.Create(const ConnName: string);
 begin
   Self.ConnName := ConnName;
+end;
+
+function TIFB_Conn.getDataBaseFileConf: string;
+begin
+  Result := FIFB_App.AppConfPath+PathDelim+'database.ini';
 end;
 
 procedure TIFB_Conn.setConnName(const Value: string);
