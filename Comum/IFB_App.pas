c88b5372f88228b6a4879b1a5857b02400dd27b2
Ismael Faustino <ismael.faustino@esyspark.com.br>
2017-11-01 02:01:36 -0200

    Nova variaveis:     SiglaProjeto     SiglaEmpresa     IDSistema     NomeLongoSistema     NomeSistema     DescricaoSistema

--- Comum/IFB_App.pas
+++ Comum/IFB_App.pas
@@ -11,17 +11,37 @@ type
     FHomePath : string;
     FAppPath : string;
     FAppConfPath : string;
+    FSiglaProjeto: string;
+    FSiglaEmpresa: string;
+    FIDSistema: Word;
+    FNomeLongoSistema: string;
+    FNomeSistema: string;
+    FDescricaoSistema: string;
     function getAppPath: string;
     procedure setAppPath(const Value: string);
     function getAppConfPath: string;
     function getAppHome: string;
     procedure setAppConfPath(const Value: string);
     procedure setAppHome(const Value: string);
+    function getSiglaProjeto: string;
+    function getSiglaEmpresa: string;
+    function getIDSistema: Word;
+    function getDescricaoSistema: string;
+    function getNomeLongoSistema: string;
+    function getNomeSistema: string;
     { Private declarations }
   public
     property AppHome : string read getAppHome write setAppHome;
     property AppPath : string read getAppPath write setAppPath;
     property AppConfPath : string read getAppConfPath write setAppConfPath;
+    property SiglaEmpresa : string read getSiglaEmpresa write FSiglaEmpresa;
+    property SiglaProjeto : string read getSiglaProjeto write FSiglaProjeto;
+    property IDSistema : Word read getIDSistema write FIDSistema;
+    property NomeSistema : string read getNomeSistema write FNomeSistema;
+    property NomeLongoSistema : string read getNomeLongoSistema write FNomeLongoSistema;
+    property DescricaoSistema : string read getDescricaoSistema write FDescricaoSistema;
+
+
     { Public declarations }
   end;
 
@@ -75,6 +95,36 @@ begin
     end;
   end;
   Result := FAppPath;
+end;
+
+function TIFB_App.getDescricaoSistema: string;
+begin
+  Result := FDescricaoSistema;
+end;
+
+function TIFB_App.getIDSistema: Word;
+begin
+  Result := FIDSistema;
+end;
+
+function TIFB_App.getNomeLongoSistema: string;
+begin
+  Result := FNomeLongoSistema;
+end;
+
+function TIFB_App.getNomeSistema: string;
+begin
+  Result := FNomeSistema;
+end;
+
+function TIFB_App.getSiglaEmpresa: string;
+begin
+  Result := FSiglaEmpresa;
+end;
+
+function TIFB_App.getSiglaProjeto: string;
+begin
+  Result := FSiglaProjeto;
 end;
 
 procedure TIFB_App.setAppConfPath(const Value: string);
