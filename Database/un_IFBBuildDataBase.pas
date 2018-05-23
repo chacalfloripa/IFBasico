462cb1afb468bd52f0f6d1695aa4f5e061eb7261
Ismael Leandro Faustino <ismael.faustino@icsul.com.br>
2018-05-23 13:53:02 -0300

    Adicionada entidade SYS_EXEC_SCRIPT. Criada assinatura do método existExecScript e addExecScript.

--- Database/un_IFBBuildDataBase.pas
+++ Database/un_IFBBuildDataBase.pas
@@ -12,6 +12,10 @@ type
   public
      procedure Build;
     { Public declarations }
+  protected
+    function existExecScript(const gruid: string):Boolean;
+    function addExecScript(const gruid: string):Boolean;
+    { Public declarations }
   end;
 
 implementation
@@ -21,8 +25,17 @@ uses
 
 { TIFB_BuildDataBase }
 
+function TIFB_BuildDataBase.addExecScript(const gruid: string): Boolean;
+begin
+
+end;
+
 procedure TIFB_BuildDataBase.Build;
 begin
+  //
+  oApp.oConn.addTable('SYS_EXEC_SCRIPT', True, False);
+  oApp.oConn.addField('SYS_EXEC_SCRIPT', 'DE_GUID', ftString, 100, True);
+  oApp.oConn.addField('SYS_EXEC_SCRIPT', 'HE_EXEC', ftDateTime, 0, True);
   //
   oApp.oConn.addTable('GEN_SITUACAO', True, False);
   oApp.oConn.addField('GEN_SITUACAO', 'NM_SITUACAO', ftString, 50, True);
@@ -50,6 +63,11 @@ begin
     end;
   end;
   //
+end;
+
+function TIFB_BuildDataBase.existExecScript(const gruid: string): Boolean;
+begin
+
 end;
 
 end.
